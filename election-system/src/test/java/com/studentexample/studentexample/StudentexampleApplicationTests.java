5127335b5397e04bba4a56369b3f0a352e1ce7a2
Pradeep Boda <149702470+Pradeep09102@users.noreply.github.com>
2025-10-25 15:19:47 -0500

    Add files via upload

---
+++ election-system/src/test/java/com/studentexample/studentexample/StudentexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentexample.studentexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
